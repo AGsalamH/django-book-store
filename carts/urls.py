2bed6a9e772bb5d971c80fe9883f0ffe79685c45
Ahmed Gamal <agsalamh@gmail.com>
2021-07-06 16:41:52 +0200

    Fix: add cart view

--- carts/urls.py
+++ carts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+
+
 urlpatterns = [
     path('', views.cart_summary, name='cart_summary'),
     path('delete/', views.cart_summary, name='cart_delete'),
--- carts/views.py
+++ carts/views.py
@@ -1,26 +1,23 @@
-from store.models import Product
-from django.http.request import HttpRequest
-from django.http.response import JsonResponse
+# Django-specific
 from django.shortcuts import render
-from .utils import Cart
+from django.http.response import JsonResponse
 from django.views.decorators.http import require_POST
 
-# Create your views here.
+# Custom
+from .utils import Cart
+from store.models import Product
 
 
 def cart_summary(request):
-    return render(request, 'carts/cart_summary.html', {
-    })
-
+    return render(request, 'carts/cart_summary.html')
 
 @require_POST
-def cart_add(request: HttpRequest):
+def cart_add(request):
     cart = Cart(request)
-
+    
     product_id = request.POST.get('product_id')
     product_qty = request.POST.get('product_qty')
-    product = Product.products.get(id=product_id)
-    cart.add(product=product, qty=product_qty)
-    return JsonResponse({
-        'test': 'data'
-    })
+    product:Product = Product.objects.get(id=product_id)
+
+    cart.add(product, qty=product_qty)
+    return JsonResponse({'qty': cart.unique_items})
