bb1ee2d51a3076a8c1b5fbee08c8071a9e12a467
Ahmed Gamal <agsalamh@gmail.com>
2021-07-07 03:20:15 +0200

    Finish Cart

--- carts/urls.py
+++ carts/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.cart_summary, name='cart_summary'),
-    path('delete/', views.cart_summary, name='cart_delete'),
-    path('update/', views.cart_summary, name='cart_update'),
+    path('delete/', views.cart_delete, name='cart_delete'),
+    path('update/', views.cart_update, name='cart_update'),
     path('add/', views.cart_add, name='cart_add'),
 ]
--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,5 @@
 # Django-specific
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http.response import JsonResponse
 from django.views.decorators.http import require_POST
 
@@ -11,13 +11,35 @@ from store.models import Product
 def cart_summary(request):
     return render(request, 'carts/cart_summary.html')
 
+
 @require_POST
 def cart_add(request):
     cart = Cart(request)
-    
+
     product_id = request.POST.get('product_id')
     product_qty = request.POST.get('product_qty')
-    product:Product = Product.objects.get(id=product_id)
-
+    product: Product = get_object_or_404(Product, id=product_id)
     cart.add(product, qty=product_qty)
-    return JsonResponse({'qty': cart.unique_items})
+    return JsonResponse({'qty': cart.unique_items, 'subtotal': cart.get_total_price()})
+
+
+@require_POST
+def cart_delete(request):
+    cart = Cart(request)
+    product_id = request.POST.get('product_id')
+    product_qty = request.POST.get('product_qty')
+    product: Product = get_object_or_404(Product, id=product_id)
+
+    cart.delete(product)
+    return JsonResponse({'qty': cart.unique_items, 'subtotal': cart.get_total_price()})
+
+
+@require_POST
+def cart_update(request):
+    cart = Cart(request)
+
+    product_id = request.POST.get('product_id')
+    product_qty = request.POST.get('product_qty')
+    product: Product = get_object_or_404(Product, id=product_id)
+    cart.update(product, qty=product_qty)
+    return JsonResponse({'qty': cart.unique_items, 'subtotal': cart.get_total_price()})
